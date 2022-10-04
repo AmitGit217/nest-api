bbc1b42421cf95ee36e1b7f0dc7c54ec1333a069
Amit Bar-Gil <amit217@yandex.com>
2022-10-04 15:01:43 +0300

    Feat: signup hash & auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,31 @@
 import { AuthInterface } from 'src/interfaces';
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthInterface) {
-    const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hash,
-      },
-    });
-    delete user.password;
-    return user;
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+      delete user.password;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
   }
 
   login() {
