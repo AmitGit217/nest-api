2c6e611158782677163fe16e4ab4ffd09a938684
Amit Bar-Gil <amit217@yandex.com>
2022-10-05 11:36:25 +0300

    Initial Auth test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,13 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthInterface } from '../src/interfaces';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,7 +19,48 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(3000);
+    prisma = app.get(PrismaService);
+    await prisma.cleanAll();
+    pactum.request.setBaseUrl('http://localhost:3000');
   });
   afterAll(() => app.close());
-  it.todo('Pass');
+
+  describe('Auth', () => {
+    const dto: AuthInterface = {
+      email: 'mail@mail.com',
+      password: '1234',
+    };
+    describe('Signup', () => {
+      it('Sign up', () => {
+        return pactum
+          .spec()
+          .post(`/auth/signup`)
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe('Login', () => {
+      it('Log in', () => {
+        return pactum
+          .spec()
+          .post(`/auth/login`)
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get current', () => {});
+    describe('Edit current', () => {});
+  });
+
+  describe('Bookmark', () => {
+    describe('Create bookmarks', () => {});
+    describe('Get bookmark', () => {});
+    describe('Get bookmark by id', () => {});
+    describe('Edit bookmark', () => {});
+    describe('Delete bookmark', () => {});
+  });
 });
